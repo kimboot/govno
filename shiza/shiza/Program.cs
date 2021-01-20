56bb9c0b6d64b935ae6f6e3e26067eb6586a990d
kimboot <39539343+kimboot@users.noreply.github.com>
2021-01-20 22:39:18 +1000

    upload

---
+++ shiza/shiza/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace project_shiza
+{
+    class Programm //Основной класс, интерфейс работы будет тут.
+    {
+        static void Main(string[] args)
+        {
+            char continu = 'д';
+            while (continu == 'д') //Цикл для продолжения работы алгоритма
+            {
+                int vibor_temi;
+                Console.WriteLine("Выберите тему : ");
+
+                vibor_temi = Convert.ToInt32(Console.ReadLine()); //Ввод темы (класса)
+                if (Convert.ToBoolean(vibor_temi = 1))
+                {
+                    Console.WriteLine("Выбран раздел 'Этектростатика'");
+                    Console.WriteLine("Выбирите формулу : "); // Здесь надо вставить экземпляр класса электростатики
+
+                }
+
+
+
+
+
+
+            }
+
+
+        }
+    }
+}
---
+++ shiza/shiza/electrostatika.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project_shiza
+{
+    class electrostatika
+    {
+        public int formula;// Здесь надо реализовать идею с формулами. Например формула кулона или др...
+        int F, Q, q, r, vecE, nu;
+        private int з = Convert.ToInt32(8.85 * Convert.ToInt32(Math.Pow(10, -12)));
+
+
+    }
+}
